cc0ea96bd17ac77efe34b97e21703c8478bbb766
juu <totkfka4@gmail.com>
2023-03-04 06:44:26 +0900

    first commit

---
+++ bucket_list/lib/main.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+//Create / Read / Update / Delete의 앞 글자를 따서 CRUD라고 부릅니다.
+//CRUD는 가장 기본이 되는 데이터 처리 기능입니다.
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class Bucket {
+  String job;
+  bool isDone;
+
+  Bucket(this.job, this.isDone);
+}
+
+/// 홈 페이지
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Bucket> bucketList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("버킷 리스트"),
+      ),
+      body: bucketList.isEmpty
+          ? Center(child: Text('버킷 리스트를 작성해 주세요.'))
+          : ListView.builder(
+              itemCount: bucketList.length, // bucketList 개수 만큼 보여주기
+              itemBuilder: (context, index) {
+                Bucket bucket = bucketList[index]; // index에 해당하는 bucket 가져오기
+                return ListTile(
+                  // 버킷 리스트 할 일
+                  title: Text(
+                    bucket.job,
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: bucket.isDone ? Colors.grey : Colors.black,
+                      decoration: bucket.isDone
+                          ? TextDecoration.lineThrough //텍스트 중앙에 선이 그어짐
+                          : TextDecoration.none,
+                    ),
+                  ),
+                  // 삭제 아이콘 버튼
+                  trailing: IconButton(
+                    icon: Icon(CupertinoIcons.delete),
+                    onPressed: () {
+                      // 삭제 버튼 클릭시
+                      print('$bucket : 삭제하기');
+                    },
+                  ),
+                  onTap: () {
+                    // 아이템 클릭시
+
+                    setState(() {
+                      bucket.isDone = !bucket.isDone; //isDone 상태변경
+                    });
+                  },
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          // + 버튼 클릭시 버킷 생성 페이지로 이동
+          String? job = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => CreatePage()),
+          );
+          if (job != null) {
+            setState(() {
+              Bucket newBucket = Bucket(job, false);
+              bucketList.add(newBucket); //버킷 리스트에 추가
+            });
+          }
+        },
+      ),
+    );
+  }
+}
+
+/// 버킷 생성 페이지
+/// 화면 갱신을 하여 에러 메시지를 띄워야 하므로 stateful로 변경
+class CreatePage extends StatefulWidget {
+  const CreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  TextEditingController textController = TextEditingController();
+
+  String? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("버킷리스트 작성"),
+        // 뒤로가기 버튼
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.chevron_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // 텍스트 입력창
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration:
+                  InputDecoration(hintText: "하고 싶은 일을 입력하세요", errorText: error),
+            ),
+            SizedBox(height: 32),
+            // 추가하기 버튼
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                child: Text(
+                  "추가하기",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  String job = textController.text;
+                  if (job.isEmpty) {
+                    setState(() {
+                      error = "내용을 입력해주세요";
+                    });
+                  } else {
+                    setState(() {
+                      error = null;
+                    });
+                    Navigator.pop(context, job); //job 변수를 반환
+                  }
+
+                  // 추가하기 버튼 클릭시
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
