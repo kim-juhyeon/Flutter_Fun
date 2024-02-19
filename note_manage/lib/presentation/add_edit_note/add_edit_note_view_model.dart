9077376e104c43725c7ca130a72200a9fdeb1991
jju <totkfka4@gmail.com>
2024-02-19 23:52:08 +0900

    클린아키텍쳐_2

---
+++ note_manage/lib/presentation/add_edit_note/add_edit_note_view_model.dart
@@ -0,0 +1,52 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:note_manage_app/domain/model/note.dart';
+import 'package:note_manage_app/domain/repository/note_repository.dart';
+import 'package:note_manage_app/presentation/add_edit_note/add_edit_note_event.dart';
+
+class AddEditNoteViewModel with ChangeNotifier {
+  final NoteRepository repository;
+
+  int _color = Colors.orange.value;
+
+  int get color => _color;
+
+  AddEditNoteViewModel(this.repository);
+
+  void onEvent(AddEditNoteEvent event) {
+    event.when(
+      changeColor: _changColor,
+      saveNote: _saveNote,
+    );
+  }
+
+  Future<void> _changColor(int color) async {
+    _color = color;
+    notifyListeners();
+  }
+
+  Future<void> _saveNote(int? id, String title, String content) async {
+    if (id == null) {
+      repository.insertNote(
+        Note(
+          title: title,
+          content: content,
+          timeStamp: DateTime.now().millisecondsSinceEpoch,
+          color: _color,
+        ),
+      );
+    } else {
+      await repository.updateNote(
+        Note(
+          id: id,
+          title: title,
+          content: content,
+          timeStamp: DateTime.now().millisecondsSinceEpoch,
+          color: _color,
+        ),
+      );
+    }
+  }
+}
